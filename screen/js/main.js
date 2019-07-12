2f02d172729c9abe72b16bacfc3a3f57fae58f49
inhs <41267204+inhs@users.noreply.github.com>
2019-07-12 20:40:22 +0700

    screen update

--- screen/js/main.js
+++ screen/js/main.js
@@ -1,43 +1,51 @@
-$(window).on('scroll', function () {
-	const scroll = $(document).scrollTop();
-	$('.JSparallaxElem').each(function () {
-		$this = $(this);
-		let direction = $(this).attr('data-direction');
-		let speed = $(this).attr('data-speed') / 3;
-		let offset = $(this).offset().top;
-		$this.css({
-			'transform': 'translateY(' + scroll / (speed * direction) + 'px)'
-		});
-	});
-	$('.JSfadeElem').each(function () {
-		let JSelOffset = $(this).offset().top;
-		if ((JSelOffset - scroll) < 600) {
-			$(this).removeClass('_disable');
-		}
-	});
-});
+function switcher() {
+	let val = $(this).attr('data-switch');
+	$('.JSswitcher').removeClass('_active');
+	$('.JSswitcher[data-switch="' + val + '"]').addClass('_active');
+	$('.JSportfolioList').removeClass('_active');
+	$('.JSportfolioList[data-switch="' + val + '"]').addClass('_active');
+	$('.JSportfolioList._active').find('.JSportfolioItems').slice(0, 4).fadeIn(0);
+};
+function openModal() {
+	let dataVal = $(this).attr('data-modal');
+	let $modal = $('.JSmodal[data-modal="' + dataVal + '"]');
+	$modal.toggleClass('_active');
+	let $iframe = $modal.find('iframe');
+	let videoSrc = $iframe.attr('src');
+	$iframe.attr('data-src', videoSrc);
+	let tempSrc = $iframe.attr('data-src');
+	if ($modal.hasClass('_active')) {
+		$iframe.attr('src', tempSrc);
+		
+	}
+	else {
+		$modal.find('iframe').removeAttr('src')
+	}
+};
 
-$(window).ready(function () {
-	$('.JStransElem').addClass('JStrans');// JStrans
 
-	$('.JSsectionMain .JSfadeElem').each(function () {
-		let delay = $(this).attr('data-delay');
-		let $this = $(this);
-		setTimeout(function () {
-			$this.removeClass('_disable');
-		}, delay);
+$(document).ready(function () {
+	$('.phoneMask').mask('+7 (000) 000 00 00')
+	$('.JSswitcher').on('click', switcher);
+	$('.JSslickSlider1').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		dots: false,
+		prevArrow: $('.prevSlide1'),
+		nextArrow: $('.nextSlide1'),
 	});
-	$('.JSsectionMain').on('mousemove', function (e) {
-		let halfWidth = $(window).width() / 2;
-		let halfHeight = $(window).height() / 2;
-		let x = e.pageX - halfWidth;
-		let y = e.pageY - halfHeight;
-		$('.JSsectionMain .JSparallaxElem').each(function () {
-			let direction = $(this).attr('data-direction');
-			let speed = $(this).attr('data-speed');
-			$(this).css({
-				'transform': 'translate(' + x / (speed * direction) + 'px, ' + y / (speed * direction) + 'px)'
-			});
-		});
+	$('.JSslickSlider2').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		dots: false,
+		prevArrow: $('.prevSlide2'),
+		nextArrow: $('.nextSlide2'),
+	});
+	$('.JSmodalToggle').on('click', openModal);
+	$('.JSportfolioItems').slice(0, 4).fadeIn(0);
+	$('.JSportfolioMoreBtn').on('click', function() {
+		$('.JSportfolioList._active').find('.JSportfolioItems:hidden').slice(0, 4).slideDown(500);
 	});
 });
