675db964457845d1fd857975113ca67a42de651d
inhs <41267204+inhs@users.noreply.github.com>
2019-07-13 16:44:46 +0700

    pdate

--- screen/core/navigation.php
+++ screen/core/navigation.php
@@ -1,26 +1,26 @@
 <ul class="navigation">
 	<li class="navigation__items">
-		<a href="3" class="navigation__link">
+		<a href="#sectionServices" class="navigation__link">
 			Услуги и цены
 		</a>
 	</li>
 	<li class="navigation__items">
-		<a href="3" class="navigation__link">
+		<a href="#sectionPortfolio" class="navigation__link">
 			Портфолио
 		</a>
 	</li>
 	<li class="navigation__items">
-		<a href="3" class="navigation__link">
+		<a href="#sectionFeatures" class="navigation__link">
 			Преимущества
 		</a>
 	</li>
 	<li class="navigation__items">
-		<a href="3" class="navigation__link">
+		<a href="#sectionReviews" class="navigation__link">
 			Отзывы
 		</a>
 	</li>
 	<li class="navigation__items">
-		<a href="3" class="navigation__link">
+		<a href="#footer" class="navigation__link">
 			Контакты
 		</a>
 	</li>
--- screen/js/main.js
+++ screen/js/main.js
@@ -4,7 +4,16 @@ function switcher() {
 	$('.JSswitcher[data-switch="' + val + '"]').addClass('_active');
 	$('.JSportfolioList').removeClass('_active');
 	$('.JSportfolioList[data-switch="' + val + '"]').addClass('_active');
-	$('.JSportfolioList._active').find('.JSportfolioItems').slice(0, 4).fadeIn(0);
+	$('.JSportfolioMoreBtn').attr('data-switch', val)
+	switch (val) {
+		case 'photo':
+			$('.JSportfolioList._active').find('.JSportfolioItems').slice(0, 6).fadeIn(0);
+			break;
+		case 'video':
+			$('.JSportfolioList._active').find('.JSportfolioItems').slice(0, 4).fadeIn(0);
+			break;
+	}
+
 };
 function openModal() {
 	let dataVal = $(this).attr('data-modal');
@@ -16,13 +25,34 @@ function openModal() {
 	let tempSrc = $iframe.attr('data-src');
 	if ($modal.hasClass('_active')) {
 		$iframe.attr('src', tempSrc);
-		
 	}
 	else {
 		$modal.find('iframe').removeAttr('src')
 	}
 };
-
+function closeOnEmpty(e) {
+	// let flag = $('.mobileMenu');
+	// if (flag.has(e.target).length === 0) {
+	// 	openModal();
+	// }
+	console.log(e.target);
+	
+};
+function slideDown() {
+	let val = $(this).attr('data-switch');
+	switch (val) {
+		case 'photo':
+			$('.JSportfolioList._active').find('.JSportfolioItems:hidden').slice(0, 3).slideDown(500);
+			break;
+		case 'video':
+			$('.JSportfolioList._active').find('.JSportfolioItems:hidden').slice(0, 2).slideDown(500);
+			break;
+	}
+};
+function videoInsert() {
+	let videoSrc = $(this).attr('data-src');
+	$(this).html('<iframe width="100%" height="100%" src="' + videoSrc + '" frameborder="0" allowfullscreen></iframe>')
+};
 
 $(document).ready(function () {
 	$('.phoneMask').mask('+7 (000) 000 00 00')
@@ -34,6 +64,16 @@ $(document).ready(function () {
 		dots: false,
 		prevArrow: $('.prevSlide1'),
 		nextArrow: $('.nextSlide1'),
+		responsive: [
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+				}
+			}
+		]
 	});
 	$('.JSslickSlider2').slick({
 		infinite: true,
@@ -42,10 +82,56 @@ $(document).ready(function () {
 		dots: false,
 		prevArrow: $('.prevSlide2'),
 		nextArrow: $('.nextSlide2'),
+		responsive: [
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+				}
+			}
+		]
+	});
+	$('.JSslickSlider3').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: false,
+		prevArrow: $('.prevSlide3'),
+		nextArrow: $('.nextSlide3'),
+		asNavFor: '.JSslickSlider3__for',
+		focusOnSelect: true,
+		centerMode: true,
+		centerPadding: '0px'
+	});
+	$('.JSslickSlider3__for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		prevArrow: $('.prevSlide3'),
+		nextArrow: $('.nextSlide3'),
+		asNavFor: '.JSslickSlider3',
+		fade: true,
+		draggable: false
+	});
+
+	var $page = $('html, body');
+	$('a[href*="#"]').click(function () {
+		$page.animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 400);
+		return false;
 	});
 	$('.JSmodalToggle').on('click', openModal);
+
 	$('.JSportfolioItems').slice(0, 4).fadeIn(0);
-	$('.JSportfolioMoreBtn').on('click', function() {
-		$('.JSportfolioList._active').find('.JSportfolioItems:hidden').slice(0, 4).slideDown(500);
-	});
+
+	$('.JSportfolioMoreBtn').on('click', slideDown);
+
+	$('.JSinsertVideo').on('click', videoInsert);
+
+	$('.JSserviceModalOpen').on('click', serviceModalOpen);
+
+	$('.JSmodal').on('click', closeOnEmpty);
 });
